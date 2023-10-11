9bd02c7c9740a0a14c8140f260210c00eefff099
Hunter Walker <hunterreesewalker@gmail.com>
2023-10-11 16:42:41 -0500

    Move build contents to root

---
+++ precache-manifest.3504b0bf20a98b5da46eaf26a2287687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660718ae16035ad2fc821da1100d1a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0a74d4a9228704b48b",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "d68951798e4cdb2de814",
+    "url": "/static/css/main.c63199a4.chunk.css"
+  },
+  {
+    "revision": "cc0a74d4a9228704b48b",
+    "url": "/static/js/2.3f887a5b.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.3f887a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68951798e4cdb2de814",
+    "url": "/static/js/main.486fea13.chunk.js"
+  },
+  {
+    "revision": "e9ce09dfc7d127585205",
+    "url": "/static/js/runtime-main.f4f93db1.js"
+  }
+]);
