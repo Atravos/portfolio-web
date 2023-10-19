4bb3a4bd83c3d62d25a1d717dd9153b81f3664ba
Hunter Walker <hunterreesewalker@gmail.com>
2023-10-18 23:28:33 -0500

    Change todo notepad description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -99,7 +99,7 @@ class Projects extends React.Component {
       },
       "To-Do Notetaking App": {
         desc:
-          "Master the art of marketing with a comprehensive organizer designed for entrepreneurs and marketing professionals",
+          "Master the art of creativity with a comprehensive organizer designed for those who believe the pen is mightier than the sword",
         techStack: "Python, Node.js, MONGODB",
         link: "https://github.com/Atravos/DiscordBot",
         open: ""
