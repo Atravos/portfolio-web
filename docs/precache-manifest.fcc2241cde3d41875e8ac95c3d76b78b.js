c6b6d1fe45578a17284d26f2d36893da02f3cd40
Hunter Walker <hunterreesewalker@gmail.com>
2023-10-22 17:45:07 -0500

    Added docs

---
+++ docs/precache-manifest.fcc2241cde3d41875e8ac95c3d76b78b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1927f198ab11a95aea38e152b454b41c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0a74d4a9228704b48b",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "bf98529002d33b15b688",
+    "url": "/static/css/main.c63199a4.chunk.css"
+  },
+  {
+    "revision": "cc0a74d4a9228704b48b",
+    "url": "/static/js/2.3f887a5b.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.3f887a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf98529002d33b15b688",
+    "url": "/static/js/main.3440e39e.chunk.js"
+  },
+  {
+    "revision": "e9ce09dfc7d127585205",
+    "url": "/static/js/runtime-main.f4f93db1.js"
+  }
+]);
